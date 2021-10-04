634c3569a6dc5b34c731fb09f592ee1ddaa2350e
Administrator <Administrator@CRG-PC-028.stairway.local>
2021-10-04 09:13:12 +0700

    coba lagi

--- resources/views/varian/index.blade.php
+++ resources/views/varian/index.blade.php
@@ -9,11 +9,7 @@
                 <div class="card">
                     <div class="card-body">
                     <div class="table-responsive">
-<<<<<<< HEAD
-                        <a href=" {{route('varian.create')}} " class="btn btn-success">Tambah Data</a><br><br>
-=======
                         <a href="{{route('varian.create')}}" class="btn btn-success">Tambah Data</a><br><br>
->>>>>>> e8cbe477fd6df2888c32fbcc804dd46c12369821
                         <table  class="table table-bordered table-sm table-hover  text-center">
                             <tr class="bg-info text-white">
                                 <th width="10px">NO.</th>
@@ -28,31 +24,17 @@
                                     <td>{{ $no+1}}</td>
                                     <td>{{ $item->nama_variant}}</td>
                                     <td>{{ $item->kode_variant}}</td>
-<<<<<<< HEAD
                                     <td>{{ $item->ukuran}} ML</td>
                                     <td>
                                         <form action="{{ route('varian.destroy',$item->id) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <a class="btn btn-primary" href="{{ route('varian.show',$item->id) }}">Edit</a>
-                                        <button type="submit" class="btn btn-danger" onclick="return confirm('Hapus Data?')">Delete</button>
-                                        </form>
-                                    </td>
-=======
-                                    <td>{{ $item->ukuran}}</td>
-                                    <td> 
-                                        <form action="{{ route('varian.destroy',$item->id) }}" method="POST">
-                                    @csrf
                                     @method('DELETE')
                                     <button class="btn btn-danger" type="submit" onclick="return confirm('Hapus Data?')">Delete</button>
                                 </form>
                                     </td>
                                     
->>>>>>> e8cbe477fd6df2888c32fbcc804dd46c12369821
                                 </tr>
                                 @endforeach
                         </table>
-                    </div>
                     </div>
 
                 </div>
